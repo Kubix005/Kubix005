7d1c11a0b2afc167df92e5b96dc6e3ac96c8df02
Kubix005 <114426203+Kubix005@users.noreply.github.com>
2024-07-19 14:36:54 +0200

    osbidian update

--- gra.py
+++ gra.py
@@ -1,15 +1,9 @@
 import pygame
-
 pygame.init()
-
 window = pygame.display.set_mode((300,300))
-
 window.fill((255,128,0))
-
 pygame.display.set_caption("Pygame - projekt")
-
 pygame.display.update()
-
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
---
+++ pygame-gra/krok 0.md
@@ -0,0 +1,5 @@
+zainstalujcie github,git,python,pygame,vsc dla gabrysia
+
+kroki:
+[[krok 1]]
+[[krok_2]]
