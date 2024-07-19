985ff699c28bb1de62ec744b703e152b95e91831
Kubix005 <114426203+Kubix005@users.noreply.github.com>
2024-07-19 14:27:56 +0200

    works now!

---
+++ gra.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+window = pygame.display.set_mode((300,300))
+
+window.fill((255,128,0))
+
+pygame.display.set_caption("Pygame - projekt")
+
+pygame.display.update()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    
