5163c6c1de81b11ec89e959fd02d7c6c06537620
Kubix005 <114426203+Kubix005@users.noreply.github.com>
2024-07-19 15:18:48 +0200

    wsad

--- gra.py
+++ gra.py
@@ -2,13 +2,15 @@ import pygame
 import sys
 
 pygame.init()
-window = pygame.display.set_mode((300, 300))
+wysokosc = 300
+dlugosc = 300
+window = pygame.display.set_mode((wysokosc,dlugosc))
 pygame.display.set_caption("Pygame - projekt")
 
 gracz = pygame.Rect(50, 50, 50, 50)
 gracz_kolor = (0, 0, 0)
 clock = pygame.time.Clock()
-rect_speed = 5
+gracz_szybkosc = 5
 
 while True:
     for event in pygame.event.get():
@@ -19,13 +21,13 @@ while True:
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_w] and gracz.top > 0:
-        gracz.y -= rect_speed
+        gracz.y -= gracz_szybkosc
     if keys[pygame.K_s] and gracz.bottom < window.get_height():
-        gracz.y += rect_speed
+        gracz.y += gracz_szybkosc
     if keys[pygame.K_a] and gracz.left > 0:
-        gracz.x -= rect_speed
+        gracz.x -= gracz_szybkosc
     if keys[pygame.K_d] and gracz.right < window.get_width():
-        gracz.x += rect_speed
+        gracz.x += gracz_szybkosc
 
     window.fill((255, 128, 0))
     pygame.draw.rect(window, gracz_kolor, gracz)
